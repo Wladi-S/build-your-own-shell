d523e62c37f1108b23b69f48f32df6b925beafd3
Wladi-S <schairer.wladimir@gmail.com>
2026-07-01 18:27:44 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
 
     while True:
 
+        built_in_commands = ["echo", "exit", "type"]
+
         sys.stdout.write("$ ")
 
         # wait for user input
@@ -12,6 +14,12 @@ def main():
 
         if command == "exit":
             break
+
+        elif command.startswith("type "):
+            if command.split("type ", 1)[1] in built_in_commands:
+                print(f"{command.split("type ", 1)[1]} is a shell builtin")
+            else:
+                print(f"{command.split("type ", 1)[1]}: not found")
 
         elif command.startswith("echo "):
             print(command.split("echo ", 1)[1])
